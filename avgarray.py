5d9d22977c7ba555a0b4df0bf0bceb7dc4f02471
Bmind <ahmedaswai@gmail.com>
2014-05-02 03:29:44 +0300

    Solving Avg Array

--- avgarray.py
+++ avgarray.py
@@ -1,6 +1,34 @@
 __author__ = 'ahmedissawi'
 
+
 def get_average():
+    data=read_input()
+    average=[]
+    for ln in data:
+        average.append(get_avg_array(ln))
+
+    return average
+
+def get_avg_array(line):
+    lst=line.split(' ')
+    total=0
+    for item in lst:
+        total+=int(item)
+    return total/len(lst)
 
 
 def read_input():
+    f = open('avgarray.txt', 'r')
+    file_contents = file.readlines(f)
+    numbers = []
+    for line in file_contents:
+        numbers.append(line.replace('0\n', '').strip())
+
+    return numbers[1:]
+
+result=get_average()
+result_out=''
+for out in result:
+    result_out+=str(out)+' '
+
+print result_out
