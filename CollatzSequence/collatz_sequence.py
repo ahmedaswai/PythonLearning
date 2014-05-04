649c8df726b9c7fb395240767fab1cf6b4865008
Bmind <ahmedaswai@gmail.com>
2014-05-04 11:57:09 +0300

    Adding Collatz Sequence Example

---
+++ CollatzSequence/collatz_sequence.py
@@ -0,0 +1,26 @@
+__author__ = 'Ahmed Issawi'
+
+
+def read_data():
+    f = open('collatz_sequence.txt', 'r')
+    file_contents = file.readlines(f)[1]
+    data = []
+    for number in file_contents.split(' '):
+        data.append(int(number))
+    return data
+
+
+def collatz_sequence():
+    result = []
+    collatz_exp = lambda num: collatz_exp(num / 2) if num % 2 == 0 else collatz_exp(3 * num + 1)
+    #collatz_count=lambda count , collatz_number : count if collatz_exp(collatz_number)==collatz_number else collatz_count(count+1,collatz_count)
+    for count in read_data():
+        result.append(collatz_exp(count))
+
+
+def main():
+    print (collatz_sequence())
+
+
+if __name__ == "__main__":
+    main()
